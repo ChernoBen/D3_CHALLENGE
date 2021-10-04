a544fc8ab62c9a7c968c833bbd854014ef7c2157
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-10-04 17:25:25 -0400

    refactor: changing function calling order

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from scripts.scripts import predict
 
 if __name__ == '__main__':
-    try:
-        days_input = int(input("Entre com o número de dias que deseja prever...\n"))
-    except ValueError:
-        print("Entrada inválida!\nEntre com um número inteiro!\n")
-    predict(days_input)
+    while True:
+        try:
+            days_input = int(input("Entre com o número de dias que deseja prever...\nEntre com '0' para sair\n"))
+            if days_input == 0:
+                break
+            predict(days_input)
+        except ValueError:
+            print("Entrada inválida!\nEntre com um número inteiro!\n")
+            break
