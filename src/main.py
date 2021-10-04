2d2e4f6cb9df438fa38705952d3c8183dfe94413
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-10-04 14:48:04 -0400

    refactor:removing empty space betwen '\n' and 'entre'

--- src/main.py
+++ src/main.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     try:
         days_input = int(input("Entre com o número de dias que deseja prever...\n"))
     except ValueError:
-        print("Entrada inválida!\n Entre com um número inteiro!\n")
+        print("Entrada inválida!\nEntre com um número inteiro!\n")
     predict(days_input)
